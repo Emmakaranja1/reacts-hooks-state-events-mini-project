41c9a64dfc6ec42eb35ba5ac5294f4321e82fa30
Emmakaranja1 <karanjaemmak@gmail.com>
2025-04-22 15:36:51 +0300

    refactor: update App component to manage selected category state and filter tasks

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,26 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+
+  const filteredTasks =
+    selectedCategory === "All"
+      ? TASKS
+      : TASKS.filter((task) => task.category === selectedCategory);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter categories={CATEGORIES} />
-      <NewTaskForm />
-      <TaskList tasks={TASKS} />
+      <CategoryFilter 
+      categories={CATEGORIES} 
+      selectedCategory={selectedCategory}
+      onCategoryChange={setSelectedCategory}
+      />
+      <NewTaskForm categories={CATEGORIES}/>
+      <TaskList tasks={filteredTasks} />
     </div>
   );
 }
