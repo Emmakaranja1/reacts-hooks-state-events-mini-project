4693bf36432cff2247715d9542376a33aacd160d
Emmakaranja1 <karanjaemmak@gmail.com>
2025-04-22 15:36:11 +0300

    refactor: enhance task management by integrating category selection and updating NewTaskForm

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories }) {
+  const categoryOptions = categories.filter((cat) => cat !== "All");
+
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState(categoryOptions[0]); 
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newTask = {
+      text,
+      category,
+    };
+    console.log("New task submitted:", newTask);
+
+    setText("");
+    setCategory(categoryOptions[0]);
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input 
+          type="text"
+          name="text" 
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+         />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select 
+        name="category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)} 
+        >
+        {categoryOptions.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
