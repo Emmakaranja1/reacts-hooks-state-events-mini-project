437f455b7634f65d11821e196aafa871cf1fba38
Emmakaranja1 <karanjaemmak@gmail.com>
2025-04-22 14:54:10 +0300

    refactor: pass tasks as props to TaskList and update Task component to accept props

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const [visibleTasks, setVisibleTasks] = useState(tasks);
+
+  function handleDelete(taskTextToRemove) {
+    const updatedTasks = visibleTasks.filter(task => task.text !== taskTextToRemove);
+    setVisibleTasks(updatedTasks);
+  }
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {visibleTasks.map((task) => (
+        <Task
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          onDelete={handleDelete}
+        />
+      ))}
     </div>
   );
 }
