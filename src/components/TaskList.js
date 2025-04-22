12b588daf8d2b2137d4419b7c374c8a649bdb238
Emmakaranja1 <karanjaemmak@gmail.com>
2025-04-22 16:54:03 +0300

    refactor: simplify TaskList component by removing local state and using onDeleteTask prop

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,24 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
 import Task from "./Task";
 
-function TaskList({ tasks }) {
-  const [visibleTasks, setVisibleTasks] = useState(tasks);
-
-  function handleDelete(taskTextToRemove) {
-    const updatedTasks = visibleTasks.filter(task => task.text !== taskTextToRemove);
-    setVisibleTasks(updatedTasks);
-  }
+function TaskList({ tasks, onDeleteTask }) {
   return (
     <div className="tasks">
-      {visibleTasks.map((task) => (
-        <Task
-          key={task.text}
-          text={task.text}
-          category={task.category}
-          onDelete={handleDelete}
-        />
-      ))}
-    </div>
+    {tasks.map((task) => (
+      <Task
+        key={task.text}
+        text={task.text}
+        category={task.category}
+        onDelete={onDeleteTask}
+      />
+    ))}
+  </div>
   );
 }
 
